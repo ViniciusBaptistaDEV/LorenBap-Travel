f1c54fc9ecfe7557f2576b108d297be8177b7b14
Vinicius Baptista <viniciusb_s@hotmail.com>
2024-08-01 15:40:54 -0300

    Adicionando Modal nos links de Login e Cadastro

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
     $('#telefone').mask('(00) 000000000');
 
+    /* Validação "Entre em contato" - INICIO*/ 
     $('#formulario').validate({
         rules:{
             nome: {
@@ -45,6 +46,95 @@ $(document).ready(function(){
         }
 
     })
+    /* Validação "Entre em contato" - FIM*/ 
+
+
+    /* Validação "Login" - INICIO*/ 
+    $('#formLogin').validate({
+        rules:{
+            emaillogin: {
+                required: true,
+                email: true
+            },
+            senha: {
+                required: true
+            }
+        },
+
+        messages: {
+            emaillogin: {
+                required: "Por favor, insira seu email.",
+                email: "Por favor, insira um email válido."
+            },
+            senha: {
+                required: "Você deve digitar sua senha para entrar."
+            }
+        },
+
+        submitHandler: function(form) {
+
+            $('#alertas-mensagens-login').html('<div class="alert alert-dismissible alert-success show">' +
+            'Seu login foi realizado com sucesso!' +
+            '<button class="btn-close" data-bs-dismiss="alert"></button>' +
+            '</div>')
+            form.reset();
+        }
+    });
+
+    $('#buttonEntrar').on('click', function(){
+        $('#formLogin').submit();
+    });
+    /* Validação "Login" - FIM*/ 
+
+
+    /* Validação "Cadastro" - INICIO*/
+    $('#formCadastro').validate({
+        rules:{
+            nomeCadastro: {
+                required: true
+            },
+            telefoneCadastro: {
+                required: true
+            },
+            emailCadastro: {
+                required: true,
+                email: true
+            },
+            senhaCadastro: {
+                required: true
+            }
+        },
+
+        messages: {
+            nomeCadastro: {
+                required: "Por favor, insira seu nome."
+            },
+            telefoneCadastro: {
+                required: "Por favor, insira seu telefone."
+            },
+            emailCadastro: {
+                required: "Por favor, insira seu email.",
+                email: "Por favor, insira um email válido."
+            },
+            senhaCadastro: {
+                required: "Você deve digitar sua senha para entrar."
+            }
+        },
+
+        submitHandler: function(form) {
+
+            $('#alertas-mensagens-cadastro').html('<div class="alert alert-dismissible alert-success show">' +
+            'Seu cadastro foi realizado com sucesso!<br>Entre com seu login e senha.' +
+            '<button class="btn-close" data-bs-dismiss="alert"></button>' +
+            '</div>')
+            form.reset();
+        }
+    });
+
+    $('#buttonCadastrar').on('click', function(){
+        $('#formCadastro').submit();
+    });
+    /* Validação "Cadastro" - FIM*/
 
 
 })
